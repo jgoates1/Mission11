20539c88f1052f47f016d6d78c00d1e178d37636
jgoates1 <jgoates1@byu.edu>
2026-03-23 17:35:49 -0600

    commit

--- backend/Mission11.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
+++ backend/Mission11.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38f7c08bc1f1e88d01c91007a4f3d699249edb9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e859537d2eddb82d317e2c15cb77c51137b4d37a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
